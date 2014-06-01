7d09b1213f4e55606688f1f61b9e66ce73ba73a5
NagaiH <nagai.h.ae@m.titech.ac.jp>
2014-06-02 00:58:57 +0900

    to calculate reverse polish fomula

--- src/calculator/Calculate.java
+++ src/calculator/Calculate.java
@@ -5,57 +5,53 @@ import java.util.Stack;
 public class Calculate {
 	Stack<Integer> numberStack = new Stack<Integer>();
 	Stack<String> operandStack = new Stack<String>();
+	Stack<String> fomulaStack = new Stack<String>(); 
 	public Calculate(String fomula){
 		int ans = 0;
-		FomulaToStack(fomula);
-		ans = DoCalculate();
-		System.out.println("total ans is " + numberStack.pop());
+		String[] items = FomulaToReversePolish(fomula);
+		ans = DoCalculate(items);
+		System.out.println("total ans is " + fomulaStack.pop());
 	}
-	//スタックに積まれた要素を用いて入力式の答えを計算
-	private int DoCalculate() {
+	//入力式の答えを計算
+	private int DoCalculate(String[] items) {
 		// TODO 自動生成されたメソッド・スタブ
-		while(!operandStack.isEmpty()){
-			int n1 = numberStack.pop();
-			int n2 = numberStack.pop();
-			int numbertoStack = 0;
-			String operand = operandStack.pop();
-			
-			System.out.println("n1 is "+ n1 + " n2 is " + n2 + " ope is " + operand);
-			
-			if(operand.equals("+")){
-				numbertoStack = plus(n1,n2);
-			}else if(operand.equals("-")){
-				numbertoStack = minus(n1,n2);
-			}else if(operand.equals("*")){
-				numbertoStack = time(n1,n2);
-			}else if(operand.equals("/")){
-				numbertoStack = devide(n1,n2);
-			}
-			
-			numberStack.push(numbertoStack);
-			System.out.println("this fomula ans is " + numbertoStack + " and pushed!!");
+		for(int i=0; i < items.length;i++){
+			int n1,n2,numbertoStack=0;
+			if(IsNumber(items[i])){
+				fomulaStack.push(items[i]);
+			}else{
+					n1 = Integer.parseInt(fomulaStack.pop());
+					n2 = Integer.parseInt(fomulaStack.pop());
+					if(items[i].equals("+")){
+						numbertoStack = plus(n1,n2);
+					}else if(items[i].equals("-")){
+						numbertoStack = minus(n1,n2);
+					}else if(items[i].equals("*")){
+						numbertoStack = time(n1,n2);
+					}else if(items[i].equals("/")){
+						numbertoStack = devide(n1,n2);
+					}
+					fomulaStack.push(Integer.toString(numbertoStack));
+				}
 		}
+			
 		return 0;
 	}
-	//入力式を逆ポーランド記法に分解し各スタックにプッシュ
-	private void FomulaToStack(String fomula) {
+	//入力文字式を項に分割し逆ポーランド記法に変換
+	private String[] FomulaToReversePolish(String fomula) {
 		String[] fomulaItems = fomula.split(" ");
-		String[] RPfomulaItems = ToReversePolish(fomulaItems);
-		//スタックにプッシュ
-		for(int i=0; i<RPfomulaItems.length;i++){
-			try{
-				numberStack.push(Integer.parseInt(RPfomulaItems[i]));
-				System.out.println(RPfomulaItems[i] + " is Number and pushed!");
-			}catch (NumberFormatException e){
-				operandStack.push(RPfomulaItems[i]);
-				System.out.println(RPfomulaItems[i] + " is operand and pushed!");
-			}
-		}
+		//逆ポーランド記法へ
+		
+		return fomulaItems;
 	}
 
-	private String[] ToReversePolish(String[] fomulaItems) {
-		// TODO 自動生成されたメソッド・スタブ
-		return fomulaItems;
+	private boolean IsNumber(String item){			
+		try{
+			Integer.parseInt(item);
+		}catch (NumberFormatException e){
+			return false;
+		}
+		return true;		
 	}
 	public static int plus(int n1, int n2){
 		return n1 + n2;
--- src/calculator/Caltest.java
+++ src/calculator/Caltest.java
@@ -4,7 +4,7 @@ public class Caltest {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		Calculate cal = new Calculate("3 + 4");
+		Calculate cal = new Calculate("10 20 30 * +");
 	}
 
 }
